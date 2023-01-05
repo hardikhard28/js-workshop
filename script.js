fa5840660c94cd70672046311fe63f05da0f216d
hardikhard28 <63140787+hardikhard28@users.noreply.github.com>
2023-01-05 20:27:05 +0530

    final

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log("loaded");
-var img = document.getElementById("img");
-var name = document.getElementById("name");
-
-console.log(img);
-fetch("https://randomuser.me/api/")
-  .then((data) => data.json())
-  .then((data) => (console.log(data)));
